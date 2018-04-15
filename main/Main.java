ac7a7a2460810a9e3bf3d63648f093ac70d75092
Simon Hamery <simon.hamery2@etu.unistra.fr>
2018-04-15 02:12:54 +0200

    Translation done (can be improved)

--- main/Main.java
+++ main/Main.java
@@ -1,9 +1,13 @@
 package main;
 
 import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import utils.aff.Couleur;
 import utils.aff.Vue;
@@ -13,17 +17,15 @@ public class Main {
 	public static void main(String s[]) {
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
-		int x = 10, y = 10, w = 800, h = 600;
-		String fileName = "data/Trioker/trio-hypo-2.csv";
+		int x = 0, y = 0, w = 1000, h = 1000;
+		String fileName1 = "data/Trioker/PiecesGagnantes.csv";
+		String fileName2 = "data/Trioker/PiecesInit.csv";
 		JFrame frame = new JFrame("Transformations dans le plan");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Vue v1, v2;
-		v1 = new Vue(w, h, fileName, false);
+		Vue v1 ;
+		v1 = new Vue(w, h, fileName1, fileName2, false);
 		v1.setBorder(BorderFactory.createLineBorder(Couleur.fg) );
-		v2 = new Vue(w, h, fileName, true);
-		v2.setBorder(BorderFactory.createLineBorder(Couleur.fg) );
 		frame.add(v1);
-		frame.add(v2, BorderLayout.EAST);
 		frame.pack();
 		frame.setLocation(x, y);
 		frame.setVisible(true);
